60681a12939cb092a54b5f67060d0378cd0782ed
Iain Entwisle <iain.e@icloud.com>
2025-07-25 14:50:27 +1000

    First Commit

---
+++ ToDo-2/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ToDo-2
+//
+//  Created by Iain Studio on 25/7/2025.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
