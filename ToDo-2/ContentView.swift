4c8a46bde230cf2fc990b0c22dd1e95fef8f321d
Iain Entwisle <iain.e@icloud.com>
2025-07-25 13:33:44 +1000

    Initial Commit

---
+++ ToDo-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDo-2
+//
+//  Created by Iain Studio on 25/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDo-2/ToDo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_2App.swift
+//  ToDo-2
+//
+//  Created by Iain Studio on 25/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
